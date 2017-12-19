7f4a550162b90764df7588cf4b970b7539fbd8d3
Kelly Clemons <kelly.clemons@mac.com>
2017-12-18 21:17:29 -0600

    minor changes in app.js

--- src/LineChartExample.js
+++ src/LineChartExample.js
@@ -1,4 +1,8 @@
-
+import React, { Component } from 'react';
+import './App.css';
+// import { scaleLinear } from 'd3-scale';
+// import { max } from 'd3-array';
+// import { select } from 'd3-selection';
 
 import { XYFrame } from 'semiotic';
 
